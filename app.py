667726a48b6acef9ac5fd940e0216fd345dd4e0c
hamzayusufmn <122660175+hamzayusufmn@users.noreply.github.com>
2025-03-19 03:08:23 +0000

    week 8 done

---
+++ app.py
@@ -0,0 +1,62 @@
+# Import necessary modules and used terminal to install packages
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'  #  URI format wasnt propley formatted so i fixed this will onnect database in my enviorment under instance
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)  
+
+#
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Todo id={self.id} title={self.title} done={self.done} created_at={self.created_at}>'
+
+# created the database and tables
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    todos = Todo.query.order_by(Todo.created_at.desc()).all()
+    return render_template('index.html', todos=todos)
+
+# add a new To-Do
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    if title:
+        new_todo = Todo(title=title)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+# a To-Do item as done
+@app.route('/toggle/<int:todo_id>')
+def toggle(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    todo.done = not todo.done
+    db.session.commit()
+    return redirect(url_for('index'))
+
+#  delete a To-Do item
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
